95149230cf0b4efba3af34996161ab137f7c2dd0
Kyle Absten <kyleabsten@gmail.com>
2018-04-12 17:46:31 -0700

    Reverted

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("This is a Git tutorial")
     }
 
     override func didReceiveMemoryWarning() {

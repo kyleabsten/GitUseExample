3e0a9dbe9cc8248dc5e17ecda19af6ef1cf62d86
Kyle Absten <kyleabsten@gmail.com>
2018-04-12 17:30:15 -0700

    Added map feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Kyle Absten on 4/12/18.
+//  Copyright © 2018 Kyle Absten. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
